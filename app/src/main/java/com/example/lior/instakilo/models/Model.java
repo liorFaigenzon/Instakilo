6566ef60b28e16cc7648359c1edc9906e245af61
kigelman.nir <kigelman.nir@gmail.com>
2016-08-15 01:50:28 +0300

    Fix sizes

--- app/src/main/java/com/example/lior/instakilo/models/Model.java
+++ app/src/main/java/com/example/lior/instakilo/models/Model.java
@@ -314,7 +314,7 @@ public class Model {
              * It is needed because large files will throw OutOfMemoryException error on calling this code
              */
             BitmapFactory.Options sizeOptions = new BitmapFactory.Options();
-            sizeOptions.inSampleSize = 8;
+            sizeOptions.inSampleSize = 1;
 
             bitmap = BitmapFactory.decodeStream(inputStream, null, sizeOptions);
             Log.d("tag","got image from cache: " + imageFileName);
